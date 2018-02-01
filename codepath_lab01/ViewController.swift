e6d9c1dd043779e8665d234692bef418c67d3557
Zheng Hao Tang <zht014@ucsd.edu>
2018-01-31 23:40:05 -0800

    Initial Commit

---
+++ codepath_lab01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  codepath_lab01
+//
+//  Created by Zheng Hao on 1/31/18.
+//  Copyright © 2018 Zheng Hao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
